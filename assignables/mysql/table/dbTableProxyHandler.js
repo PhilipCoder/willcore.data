083c31306e399e7d173cf5ce59181f2ff4d4d80c
Philip Schoeman <philip.johannes.schoeman@gmail.com>
2019-12-04 16:57:45 +0200

    dowk

--- assignables/mysql/table/dbTableProxyHandler.js
+++ assignables/mysql/table/dbTableProxyHandler.js
@@ -1,9 +1,23 @@
 const assignableProxyHandler = require("../../../proxies/base/assignableProxyHandler.js");
+const columnProxy = require("../column/dbColumnProxy.js");
 
 class dbTableProxyHandler extends assignableProxyHandler {
   constructor() {
     super();
+    this.setTraps.unshift(this.assignStraightValue);
   }
+  assignStraightValue(target, property, value, proxy) {
+    if (value instanceof columnProxy && target[property] instanceof columnProxy) {
+      let targetColumnName = value._dbColumnAssignable.columnInfo.name;
+      let targetTableName = value._dbColumnAssignable.parentProxy._dbTableAssignable.tableInfo.name;
+      target[property]._dbColumnAssignable.parentProxy._dbColumnAssignable.columnInfo.reference = {
+        table: targetTableName,
+        column: targetColumnName
+    };
+        return { value: true };
+    }
+    return { value: false, status: false };
+}
 }
 
 module.exports = dbTableProxyHandler;
